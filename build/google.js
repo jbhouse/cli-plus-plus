06e0df3e7a970b7f97dbc4a9b0863cfa2331df71
jbhouse <jbmoorhouse1@gmail.com>
2020-02-10 22:18:35 -0600

    removed the need for opn dependency, also limited this package to only work on windows machines

--- build/google.js
+++ build/google.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
 (() => {
-  require('opn')(`https://www.google.com/search?q=${process.argv.slice(2).join('+')}`);
+  require('child_process').exec(`rundll32 url.dll, FileProtocolHandler https://www.google.com/search?q=${process.argv.slice(2).join('+')}`, (err, stdout, stderr) => {
+    if (err) {
+      console.log('Error: ', err);
+      return;
+    }
+    stdout ? console.log(stdout) : console.log(stderr);
+  });
 })();
--- build/openPR.js
+++ build/openPR.js
@@ -3,5 +3,13 @@
 (async () => {
   let pullRequestUrl = require('./gitUrl').gitUrl();
   pullRequestUrl += '\\pull\\new\\' + await require('./parseBranchNameFromList').getCurrentBranch();
-  require('opn')(pullRequestUrl);
+  (() => {
+    require('child_process').exec(`rundll32 url.dll, FileProtocolHandler ${pullRequestUrl}`, (err, stdout, stderr) => {
+      if (err) {
+        console.log('Error: ', err);
+        return;
+      }
+      stdout ? console.log(stdout) : console.log(stderr);
+    });
+  })(pullRequestUrl);
 })();
