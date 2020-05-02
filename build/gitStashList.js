b21d9590b80830dfbc589fb7ced375df7af490b5
jbhouse <jbmoorhouse1@gmail.com>
2020-05-02 16:36:49 -0500

    added alias for git stash list

---
+++ build/gitStashList.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+(() => {
+    const branchName = process.argv[2] ? process.argv[2] : 'develop';
+    require('child_process').exec('git stash list', { cwd: process.cwd() }, (err, stdout, stderr) => {
+        if (err) {
+            console.log('Error: ', err);
+            return;
+        }
+        stdout ? console.log(stdout) : console.log(stderr);
+    });
+})();
