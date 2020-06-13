151e57b65f4a1d2c684fb1f6833225855776f0ff
jbhouse <jbmoorhouse1@gmail.com>
2020-06-13 17:30:50 -0500

    updated git commit command and added the ability to copy current branch name to clipboard

--- build/gitCommit.js
+++ build/gitCommit.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 (() => {
-  require('child_process').exec(`git commit . -m "${process.argv.slice(2).join(' ')}"`, { cwd: process.cwd() }, (err, stdout, stderr) => {
+  require('child_process').exec(`git commit -m "${process.argv.slice(2).join(' ')}"`, { cwd: process.cwd() }, (err, stdout, stderr) => {
     if (err) {
       console.log('Error: ', err);
       return;
