d8c61ab6a276fce7fe26f491de11fcae58f020d4
jb <jbmoorhouse1@gmail.com>
2021-07-27 23:57:23 -0500

    updated hub command file path to be OS agnostic

--- build/gitUrl.js
+++ build/gitUrl.js
@@ -2,7 +2,8 @@
 
 module.exports = {
   gitUrl: function gitUrl() {
-    const gitConfigContents = require('fs').readFileSync(`${process.cwd()}\\.git\\config`).toString().split('\n');
+    const pth = path.join(".git", "config");
+    const gitConfigContents = require('fs').readFileSync(`${process.cwd()}pth`).toString().split('\n');
     const githubUrl = gitConfigContents[(gitConfigContents.indexOf('[remote "origin"]') + 1)].trim().split('= ')[1];
     return githubUrl.substring(0, githubUrl.length - 4);
   },
